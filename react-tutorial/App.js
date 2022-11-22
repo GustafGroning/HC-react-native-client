d0712b82b113e479ea1f904ab2ef8b6d0d426372
Gustaf Gröning <gustafgroning@gmail.com>
2022-11-22 19:00:31 +0100

    commit

---
+++ react-tutorial/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  const [name, setName] = useState("Gustaf");
+
+  const clickHandler = () => {
+    setName("Alba");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text> My name is {name} </Text>
+      <Text> </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={clickHandler} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+});
